728822bbb4261b2da9f055feaf29ef14b279a42b
quantumconduit <quantumconduit@proton.me>
2025-11-26 11:43:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.168.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.168.0/examples/jsm/controls/OrbitControls.js';
 
 import {
   primes,
